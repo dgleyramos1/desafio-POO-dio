1266a43a0fa0dd963d3f8182b092eebc73055b9b
Dgley Ramos <dgleysilva@gmail.com>
2022-01-18 07:02:39 -0300

    Colocando a regra de negocio nos métodos

--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -2,18 +2,32 @@ package br.com.dio.desafio.dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
     private String nome;
-    private Set<Conteudo> conteudoInscrito = new LinkedHashSet<>();
-    private Set<Conteudo> conteudoConcluido =  new LinkedHashSet<>();
+    private Set<Conteudo> conteudoInscritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudoConcluidos =  new LinkedHashSet<>();
 
-    public void inscreverBootcamp(Bootcamp bootcamp){}
+    public void inscreverBootcamp(Bootcamp bootcamp){
+        this.conteudoInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
+    }
 
-    public void progredir(){}
+    public void progredir(){
+        Optional<Conteudo> conteudo = this.conteudoInscritos.stream().findFirst();
+        if(conteudo.isPresent()){
+            this.conteudoConcluidos.add(conteudo.get());
+            this.conteudoInscritos.remove(conteudo.get());
+        } else{
+            System.err.println("você não está matriculado em nenhum conteúdo!");
+        }
+    }
 
-    public void calcularTotalXp(){}
+    public double calcularTotalXp(){
+        return this.conteudoConcluidos.stream().mapToDouble(Conteudo::calcularXp).sum();
+    }
 
     public String getNome() {
         return nome;
@@ -24,19 +38,19 @@ public class Dev {
     }
 
     public Set<Conteudo> getConteudoInscrito() {
-        return conteudoInscrito;
+        return conteudoInscritos;
     }
 
     public void setConteudoInscrito(Set<Conteudo> conteudoInscrito) {
-        this.conteudoInscrito = conteudoInscrito;
+        this.conteudoInscritos = conteudoInscrito;
     }
 
     public Set<Conteudo> getConteudoConcluido() {
-        return conteudoConcluido;
+        return conteudoConcluidos;
     }
 
     public void setConteudoConcluido(Set<Conteudo> conteudoConcluido) {
-        this.conteudoConcluido = conteudoConcluido;
+        this.conteudoConcluidos = conteudoConcluido;
     }
 
     @Override
@@ -44,11 +58,11 @@ public class Dev {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Dev dev = (Dev) o;
-        return Objects.equals(nome, dev.nome) && Objects.equals(conteudoInscrito, dev.conteudoInscrito) && Objects.equals(conteudoConcluido, dev.conteudoConcluido);
+        return Objects.equals(nome, dev.nome) && Objects.equals(conteudoInscritos, dev.conteudoInscritos) && Objects.equals(conteudoConcluidos, dev.conteudoConcluidos);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(nome, conteudoInscrito, conteudoConcluido);
+        return Objects.hash(nome, conteudoInscritos, conteudoConcluidos);
     }
 }
